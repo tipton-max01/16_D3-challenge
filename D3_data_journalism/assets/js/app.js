92afa38d7a91d5ee2e8fbf0d991585453c14e2e9
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 14:33:11 -0500

    extracted healthcare from array

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -11,10 +11,10 @@ var margin = {
 
 // append the svg object to the body of the page
 var svg = d3.select("scatter")
-  .append("svg")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
@@ -23,21 +23,39 @@ var svg = d3.select("scatter")
 d3.csv("./assets/data/data.csv").then(function(data) {
 
     console.log(data);
+
+    //log healthcare list
+    var healthcare = data.map(data => data.healthcare);
+    console.log("healthcare", healthcare);
+
 }).catch(function(error) {
     console.log(error);
 });
 
-    // Add X axis
-    // var x = d3.scaleLinear()
-        .domain([0, 4000])
-        .range([ 0, width ]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+    // // Add X axis
+    // // var x = d3.scaleLinear()
+    //     .domain([0, 4000])
+    //     .range([ 0, width ]);
+    // svg.append("g")
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x));
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, 500000])
-        .range([ height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+    // // Add Y axis
+    // // var y = d3.scaleLinear()
+    //     .domain([0, 500000])
+    //     .range([ height, 0]);
+    // svg.append("g")
+    //     .call(d3.axisLeft(y));
+
+    // // Add dots
+    // svg.append('g')
+    //     .selectAll("dot")
+    //     .data(data)
+    //     .enter()
+    //     .append("circle")
+    //         .attr("cx", function (d) { return x(d.GrLivArea); } )
+    //         .attr("cy", function (d) { return y(d.SalePrice); } )
+    //         .attr("r", 1.5)
+//     //         .style("fill", "#69b3a2")
+
+// })
