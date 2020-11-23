c38f23c84a255aa66a54da1f93ba645e685f9f35
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 18:56:14 -0500

    attempting display state names to circle plots

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -2,14 +2,14 @@
 // @TODO: YOUR CODE HERE!
 
 // set the dimensions and margins of the graph
-var svgWidth = 960;
+var svgWidth = 800;
 var svgHeight = 500;
 
 var margin = {
-    top: 10, 
-    right: 30, 
-    bottom: 30, 
-    left: 60
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -32,11 +32,11 @@ d3.csv("./assets/data/data.csv").then(function(data) {
 
     // Step 2: Create scale functions
     var xScale = d3.scaleLinear()
-        .domain([20, d3.max(data, d => d.poverty)])
+        .domain([17, d3.max(data, d => d.poverty)])
         .range([0, width]);
 
     var yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.healthcare)])
+        .domain([4.4, d3.max(data, d => d.healthcare)])
         .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -53,26 +53,42 @@ d3.csv("./assets/data/data.csv").then(function(data) {
 
     // Step 5: Create Circles
     var circlesGroup = chartGroup.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.poverty))
-    .attr("cy", d => yScale(d.healthcare))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
-
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.poverty), )
+        .attr("cy", d => yScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "lightblue")
+        .attr("opacity", ".5");
+    
     // Step 6: Initialize tool tip
     var toolTip = d3.tip()
         .attr("class", "d3-tip")
-        .offset([80, -100])
+        .offset([80, -60])
+        .style("position", "absolute")
+        .style("pointer-events", "none")
         .html(function(d) {
-            return (`${d.state}<br>Poverty (%): ${d.poverty}<br>Healthcare (%): ${d.healthcare}`)
+            return (`${d.state}<br>In Poverty (%): ${d.poverty}<br>Lacks Healthcare (%): ${d.healthcare}`)
         });      
 
     // Step 7: Create tooltip in the chart
     chartGroup.call(toolTip);
 
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 0)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+    
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 10})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+      
 
     }).catch(function(error) {
             console.log(error);
