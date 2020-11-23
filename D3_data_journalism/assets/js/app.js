b4e06646c140648120f6415224524f34ba871db4
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 14:13:44 -0500

    add X axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,7 +1,11 @@
 // @TODO: YOUR CODE HERE!
 
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+var margin = {
+    top: 10, 
+    right: 30, 
+    bottom: 30, 
+    left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -15,7 +19,6 @@ var svg = d3.select("scatter")
           "translate(" + margin.left + "," + margin.top + ")");
 
 
-
 // Load data from data.csv
 d3.csv("./assets/data/data.csv").then(function(data) {
 
@@ -23,3 +26,11 @@ d3.csv("./assets/data/data.csv").then(function(data) {
 }).catch(function(error) {
     console.log(error);
 });
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0, 4000])
+        .range([ 0, width ]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
