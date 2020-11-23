b7b84759db0ddc82e67230891a82cb8d38e21df1
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 16:36:29 -0500

    simplifying dimensions/margins

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,13 +1,18 @@
 // @TODO: YOUR CODE HERE!
 
 // set the dimensions and margins of the graph
+var svgWidth = 960;
+var svgHeight = 500;
+
 var margin = {
     top: 10, 
     right: 30, 
     bottom: 30, 
-    left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("scatter")
@@ -37,10 +42,9 @@ d3.csv("./assets/data/data.csv").then(function(data) {
     .range([height, 0]);
 
     // scale x to chart width
-    var xScale = d3.scaleBand()
-    .domain(poverty)
-    .range([0, width])
-    .padding(0.05);
+    var xScale = d3.scaleLinear()
+    .domain([0, d3.max(poverty)])
+    .range([0, width]);
 
     // create axes
     var yAxis = d3.axisLeft(yScale);
@@ -61,6 +65,22 @@ d3.csv("./assets/data/data.csv").then(function(data) {
     svg.append("g")
         .call(d3.axisLeft(yAxis));
 
+    // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+    // Its opacity is set to 0: we don't see it by default.
+    // var toolTip = d3.select("scatter")
+    //     .append("div")
+    //     .style("opacity", 50)
+    //     .attr("class", "tooltip")
+    //     .style("background-color", "white")
+    //     .style("border", "solid")
+    //     .style("border-width", "1px")
+    //     .style("border-radius", "5px")
+    //     .style("padding", "10px")
+
+    var toolTip = d3.select("scatter")
+    .append("div")
+        .attr("class", "tooltip")
+
     // Add dots
     svg.append('g')
         .selectAll("dot")
@@ -71,6 +91,11 @@ d3.csv("./assets/data/data.csv").then(function(data) {
             .attr("cy", function (d) { return y(d.healthcare); } )
             .attr("r", 1.5)
             .style("fill", "#69b3a2")
+
+
+
+// {/* <img src="/static/image.svg"> */}
+
 
     }).catch(function(error) {
             console.log(error);
