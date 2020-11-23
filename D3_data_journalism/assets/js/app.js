4b951ac2f20946812af4727f659582b3966c51e5
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 13:56:09 -0500

    set dimensions & margins

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,5 +1,14 @@
 // @TODO: YOUR CODE HERE!
 
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+
+
+
+
 // Load data from data.csv
 d3.csv("./assets/data/data.csv").then(function(data) {
 
