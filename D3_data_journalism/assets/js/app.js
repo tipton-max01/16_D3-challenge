5389410298b3c57546781601d27eb6ad4cb3b7df
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 13:21:09 -0500

    success load in csv

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,13 +1,7 @@
 // @TODO: YOUR CODE HERE!
 
-// // Load data from hours-of-tv-watched.csv
-// d3.csv("./hours-of-tv-watched.csv").then(function(tvData) {
-
-//     console.log(tvData);
-  
-
 // Load data from data.csv
-d3.csv("./data/data.csv").then(function(data) {
+d3.csv("./assets/data/data.csv").then(function(data) {
 
     console.log(data);
 }).catch(function(error) {
