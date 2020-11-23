a9a6258fdbf155e44fddefd932263f0d33d25ea4
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 14:01:40 -0500

    rename div to scatter in js app

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,7 +6,7 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+var svg = d3.select("scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
