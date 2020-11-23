e78732e78cc309405fd64db709141cdf60765d75
Max Tipton <bartholdy9000@tippyPro15.fios-router.home>
2020-11-23 14:14:58 -0500

    add y axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -28,9 +28,16 @@ d3.csv("./assets/data/data.csv").then(function(data) {
 });
 
     // Add X axis
-    var x = d3.scaleLinear()
+    // var x = d3.scaleLinear()
         .domain([0, 4000])
         .range([ 0, width ]);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, 500000])
+        .range([ height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
